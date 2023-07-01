4b27121af034afb6fdc0dd0a0a6e0396b8d2f91f
Hannn04 <22523162@students.uii.ac.id>
2023-07-02 06:19:14 +0700

    Tambahan

--- src/Informasi/InformasiController.java
+++ src/Informasi/InformasiController.java
@@ -3,17 +3,34 @@ package Informasi;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class InformasiController implements Initializable{
 
+    @FXML
+    private Button winongo;
+
+    private void handleButtonAction (ActionEvent event) throws Exception {
+        Stage stage;
+        Parent root;
+
+        stage = (Stage) winongo.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Winongo.fxml"));
     
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        
         
     }
 
