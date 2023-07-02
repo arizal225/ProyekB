5e8ad9a85245bb34b2a8d236482720b05296f667
Hannn04 <22523162@students.uii.ac.id>
2023-07-03 00:17:00 +0700

    Tambah

---
+++ src/Informasi/WinongoController.java
@@ -0,0 +1,47 @@
+package Informasi;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class WinongoController implements Initializable{
+
+    @FXML
+    private Button kembali;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void switchToInformasi (ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Informasi.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+
+
+    
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+
+}
