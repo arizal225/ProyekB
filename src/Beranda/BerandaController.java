b66685231114f69bb766cbe9cfaa0b5e3cb75010
Hannn04 <22523162@students.uii.ac.id>
2023-07-04 06:36:47 +0700

    Tambah

--- src/Beranda/BerandaController.java
+++ src/Beranda/BerandaController.java
@@ -53,6 +53,15 @@ public class BerandaController implements Initializable {
         stage.setMaximized(true);
         stage.show();
     }
+
+    @FXML
+    private void switchToLogin(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("../Login/Login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
     
 
