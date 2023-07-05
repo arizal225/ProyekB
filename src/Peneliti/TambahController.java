f8ae7cdeeb44ac90eaaa517ad9429f1aba4ba8a1
Hannn04 <22523162@students.uii.ac.id>
2023-07-05 09:02:57 +0700

    Tambah

--- src/Peneliti/TambahController.java
+++ src/Peneliti/TambahController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -12,7 +13,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class TambahController {
+public class TambahController implements Initializable{
 
     @FXML
     private Button button;
@@ -102,6 +103,12 @@ public class TambahController {
     @FXML
     void hapus(ActionEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
     }
 
 }
