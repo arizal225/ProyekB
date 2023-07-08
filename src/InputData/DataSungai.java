5fae1cf916de7814c505046d4ae5da8019feb0d4
Hannn04 <22523162@students.uii.ac.id>
2023-07-08 10:32:46 +0700

    Tambahan

--- src/InputData/DataSungai.java
+++ src/InputData/DataSungai.java
@@ -1,23 +1,55 @@
 package InputData;
 
 public class DataSungai {
-    String Nama;
-    String Kontak;
+    String kolomNama;
+    String kolomLokasi;
+    String kolomSuhu;
+    String kolomLuas;
+    String kolomPanjang;
+    String kolomWarna;
+    String kolomKedalaman;
+    String kolomTahun;
 
-    public DataSungai(){
-        
+    public DataSungai(String x, String y, String z, String i, String j, String k, String l, String m){
+        this.kolomNama = x;
+        this.kolomLokasi = y;
+        this.kolomSuhu = z;
+        this.kolomLuas = i;
+        this.kolomPanjang = j;
+        this.kolomWarna = k;
+        this.kolomKedalaman = l;
+        this.kolomTahun = m;
     }
-    
-    public DataSungai(String x, String y){
-        this.Nama = x;
-        this.Kontak = y;
+
+    public String getkolomNama(){
+        return kolomNama;
     }
-    
-    public String getNama(){
-        return Nama;
+
+    public String getkolomLokasi(){
+        return kolomLokasi;
     }
-    
-    public String getKontak(){
-        return Kontak;
+
+    public String getkolomSuhu(){
+        return kolomSuhu;
+    }
+
+    public String getkolomLuas(){
+        return kolomSuhu;
+    }
+
+     public String getkolomPanjang(){
+        return kolomPanjang;
+    }
+
+    public String getkolomWarna(){
+        return kolomWarna;
+    }
+
+    public String getkolomKedalaman(){
+        return kolomKedalaman;
+    }
+
+    public String getkolomTahun(){
+        return kolomTahun;
     }
 }
--- src/InputData/InputDataController.java
+++ src/InputData/InputDataController.java
@@ -3,6 +3,7 @@ package InputData;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,6 +15,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class InputDataController implements Initializable{
+
+    ObservableList data = observableArrayList(
+    new DataSungai ("Winongo", "apa", "4 Hari", "13", "Terima"),
+    new DataSungai ("MCDonald'", "Fish Burger", "3 Hari", "28", "Tolak"),
+    new DataSungai ("Rotiboy", "Choco Lava Boy", "3 Hari", "17", "Tolak"),
+    new DataSungai ("Burger King", "Cheese Whopper", "3 Hari", "18", "Terima"),
+    new DataSungai ("RotiO", "Chocolate Pastry", "3 Hari", "19", "Tolak")
+);
 
     @FXML
     private Button button;
@@ -52,46 +61,81 @@ public class InputDataController implements Initializable{
     private TableView<DataSungai> tableView;
 
     @FXML
-    private TextField textField1;
+    private TextField lokasi;
 
     @FXML
-    private TextField textField2;
+    private TextField luas;
 
     @FXML
-    private TextField textField21;
+    private TextField nama;
 
     @FXML
-    private TextField textField22;
+    private TextField panjang;
 
     @FXML
-    private TextField textField221;
+    private TextField suhu;
 
     @FXML
-    private TextField textField2211;
+    private TextField kedalaman;
 
     @FXML
-    private TextField textField2212;
-
-    @FXML
-    private TextField textField2213;
+    private TextField tahun;
 
     @FXML
     private TextField textFieldHapus;
 
     @FXML
+    private TextField warna;
+
+    @FXML
     private void handleButtonAction(ActionEvent event, Object data) {
+    String Temp1 =  nama.getText();
+    String Temp2 =  lokasi.getText();
+    String Temp3 =  suhu.getText();
+    String Temp4 =  luas.getText();
+    String Temp5 =  panjang.getText();
+    String Temp6 =  warna.getText();
+    String Temp7 =  kedalaman.getText();
+    String Temp8 =  tahun.getText();
+ 
+
+    data.add(new DataSungai(Temp1, Temp2, Temp3, Temp4, Temp5, Temp7, Temp8));
+        nama.setText("");
+        lokasi.setText("");
+        suhu.setText("");
+        luas.setText("");
+        panjang.setText("");
+        warna.setText("");
+        kedalaman.setText("");
+        tahun.setText("");
     
+    
+    }
+
+    private ObservableList observableArrayList(DataSungai dataSungai, DataSungai dataSungai2, DataSungai dataSungai3,
+            DataSungai dataSungai4, DataSungai dataSungai5) {
+        return null;
     }
 
     @FXML
     void hapus(ActionEvent event) {
-
+        int ke_brp = Integer.parseInt(textFieldHapus.getText());
+        int x = ke_brp - 1;
+        data.remove(x);
+        textFieldHapus.setText("");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+        kolomNama.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("NamaSungai"));
+        kolomLokasi.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("LokasiSungai"));
+        kolomSuhu.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("SuhuSungai"));
+        kolomLuas.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("LuasSungai"));
+        kolomPanjang.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("PanjangSungai"));
+        kolomWarna.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("WarnaSungai"));
+        kolomKedalaman.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("KedalamanSungai"));
+        kolomTahun.setCellValueFactory(new PropertyValueFactory<DataSungai, String>("TahunSungai"));
+        tableView.setItems(data);
     }
 
 }
